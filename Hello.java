adbfb514f118085f6a160e1177f1c95b1ab14f17
Telity <nigu0001@stud.kea.dk>
2024-04-30 20:38:26 +0200

    Updated

--- Hello.java
+++ Hello.java
@@ -1,5 +1,25 @@
+import java.util.ArrayList;
+
 public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello");
+    private int age;
+    private String name;
+    private boolean activity;
+    private boolean competitive;
+
+    public void setAge(int age){
+
     }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+public class Yolo{
+
+    ArrayList<Hello> members = new ArrayList<Hello>()
 }
